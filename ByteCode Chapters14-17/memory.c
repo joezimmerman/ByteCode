b7d246e53fb132352a71e9a6ca2453ab12e5d8e2
joezimmerman <153047449+joezimmerman@users.noreply.github.com>
2023-12-07 23:31:20 -0600

    Delete ByteCode Chapters14-17 directory

--- ByteCode Chapters14-17/memory.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include "memory.h"
-void* reallocate(void* point, size_t old, size_t new) {
-  if (new == 0) {free(point); return NULL;}
-  void* result = realloc(point, new);
-  if(result == NULL) exit(1);
-  return result;
-}
